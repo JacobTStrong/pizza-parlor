fc02dd1db63cb8e3e34645bc5b7002df02b2c282
Jacob Strong <rjts2896@gmail.com>
2021-07-21 16:00:13 -0700

    add script for selecting size

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,16 @@ function Pizza(size, crust, sauce, toppings) {
   this.sauce = sauce;
   this.toppings = toppings;
 }
+
+Pizza.prototype.orderTotal = function() {
+  let total = 0;
+  if (this.size === 'small') {
+    this.total = 8;
+  } else if (this.size === 'medium') {
+    this.total = 11;
+  } else if (this.size === 'large') {
+    this.total = 14;
+  } else {
+    this.total = 19;
+  }
+};
