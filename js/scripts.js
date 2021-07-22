7f70362972cd5b149ee41a3e7842a1544d8db671
Jacob Strong <rjts2896@gmail.com>
2021-07-21 23:40:37 -0700

    add final scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-function Pizza(size, crust, sauce, toppings) {
+// Business Logic
+
+function Pizza(size, crust, sauce) {
   this.size = size;
   this.crust = crust;
   this.sauce = sauce;
-  this.toppings = toppings;
+  this.total = 0;
 }
 
 Pizza.prototype.orderTotal = function() {
-  let total = 0;
   if (this.size === 'small') {
     this.total = 8;
   } else if (this.size === 'medium') {
@@ -28,4 +29,84 @@ Pizza.prototype.orderTotal = function() {
   } else {
     this.total +=2;
   }
+  return this.total;
 };
+
+// User Interface Logic
+
+$(document).ready(function() {
+  $("#pizzaForm").submit(function(event) {
+    event.preventDefault();
+    $("#pizzaToppings").fadeToggle();
+    $("#outroPartOne").fadeToggle();
+
+    let finalSize = $("#sizeSelection").val();
+    let finalCrust = $("#crustSelection").val();
+    let finalSauce = $("#sauceSelection").val();
+
+    let usersPizza = new Pizza(finalSize, finalCrust, finalSauce);
+    
+    $("#totalCost").text("$" + usersPizza.orderTotal());
+  });
+});
+
+$(document).ready(function() {
+  $(".button#startCooking").click(function() {
+    $("#enterTheTraveler").fadeToggle();
+		$("#header").fadeToggle();
+  });
+
+  $(".button#toOriginStory").click(function() {
+    $("#originStory").fadeToggle();
+		$("#enterTheTraveler").fadeToggle();
+  });
+
+  $(".button#buildPizza").click(function() {
+    $("#endIntro").fadeToggle();
+		$("#originStory").fadeToggle();
+  });
+
+  $(".button#startAssembling").click(function() {
+    $("#pizzaSize").fadeToggle();
+		$("#endIntro").fadeToggle();
+  });
+
+  $(".button#sizeSelected").click(function() {
+    $("#pizzaCrust").fadeToggle();
+		$("#pizzaSize").fadeToggle();
+  });
+
+  $(".button#crustSelected").click(function() {
+    $("#pizzaSauce").fadeToggle();
+		$("#pizzaCrust").fadeToggle();
+  });
+
+  $(".button#sauceSelected").click(function() {
+    $("#pizzaToppings").fadeToggle();
+		$("#pizzaSauce").fadeToggle();
+  });
+
+  $(".button#outroButtonOne").click(function() {
+    $("#outroPartTwo").fadeToggle();
+		$("#outroPartOne").fadeToggle();
+  });
+
+  $(".button#outroButtonTwo").click(function() {
+    $("#outroPartThree").fadeToggle();
+		$("#outroPartTwo").fadeToggle();
+  });
+
+  $(".button#outroButtonThree").click(function() {
+    $("#outroPartFour").fadeToggle();
+		$("#outroPartThree").fadeToggle();
+  });
+
+  $(".button#outroButtonFour").click(function() {
+    $("#outroPartFive").fadeToggle();
+		$("#outroPartFour").fadeToggle();
+  });
+
+  $(".button#refreshPage").click(function() {
+    location.reload()
+  });
+});
